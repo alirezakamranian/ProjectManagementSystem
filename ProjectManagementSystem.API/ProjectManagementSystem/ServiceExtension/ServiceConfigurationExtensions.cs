00e1ced52c439ad1a4a30d065269e6ba6d1fb9bf
alirezakamranian <kamranianalireza1090@gmail.com>
2024-02-28 05:23:51 -0800

    Feat:Add services extension

---
+++ ProjectManagementSystem.API/ProjectManagementSystem/ServiceExtension/ServiceConfigurationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Application.Services.EmployeeService;
+using Domain.Services;
+using Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace ProjectManagementSystem.ServiceExtension
+{
+    public static class ServiceConfigurationExtensions
+    {
+        //Mvc
+        public static void ConfigureMvc(this IServiceCollection services) =>
+            services.AddControllers();
+        //DbContext
+        public static void ConfigureDbContext(this IServiceCollection services, WebApplicationBuilder builder) =>
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+        //Swagger
+        public static void ConfigureSwagger(this IServiceCollection services) 
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+        //EmployeeService
+        public static void ConfigureEmployeeService(this IServiceCollection services) =>
+            services.AddTransient<IEmployeeService, EmployeeService>();
+    }
+}
