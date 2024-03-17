e9f4a7d01097a1efd68a83205fb3548455f08c52
alirezakamranian <kamranianalireza1090@gmail.com>
2024-03-17 03:31:45 -0700

    Feat:Add OrganizationInvitation Reject service and endpoint

---
+++ ProjectManagementSystem.API/Domain/Models/Dtos/OrganizationInvitation/Response/RejectInvitationRespons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Dtos.OrganizationInvitation.Response
+{
+    public class RejectInvitationRespons
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ ProjectManagementSystem.API/Domain/Models/ServiceResponses/OrganizationInvitation/RejectOrganizationInvitationServiceResponse.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models.ServiceResponses.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.ServiceResponses.OrganizationInvitation
+{
+    public class RejectOrganizationInvitationServiceResponse(string status):ServiceResponseBase(status)
+    {
+    }
+    public class RejectOrganizationInvitationServiceResponseStatus:ServiceResponseStatusBase
+    {
+        public const string NotificationNotExists = "NotificationNotExists";
+    }
+}
