35266b2e5107f26ea280acf0746a7c238f89dd16
alirezakamranian <kamranianalireza1090@gmail.com>
2024-04-23 12:03:17 -0700

    Ref:Refactor createTaskList service response

--- ProjectManagementSystem.API/Domain/Models/Dtos/ProjectTaskList/Response/CreateTaskListResponse.cs
+++ ProjectManagementSystem.API/Domain/Models/Dtos/ProjectTaskList/Response/CreateTaskListResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Models.ServiceResponses.ProjectTaskList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,6 @@ namespace Domain.Models.Dtos.ProjectTaskList.Response
     public class CreateTaskListResponse
     {
         public string Message { get; set; }
+        public MinimumValueTaskListDto TaskList { get; set; }
     }
 }
