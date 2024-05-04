2f445569867b2cdb2949acc9f51ffc4bd9195e40
alirezakamranian <kamranianalireza1090@gmail.com>
2024-05-04 09:38:12 -0700

    Feat:Add StorageService with upload operation

---
+++ ProjectManagementSystem.API/Domain/Models/InternalSerives/Storage/Request/UploadFileRequest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.InternalSerives.Storage.Request
+{
+    public class UploadFileRequest
+    {
+        public IFormFile File { get; set; }
+        public string Key { get; set; }
+    }
+}
---
+++ ProjectManagementSystem.API/Domain/Services/InternalServices/IStorageService.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models.InternalSerives.Storage.Request;
+using Domain.Models.ServiceResponses.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services.InternalServices
+{
+    public interface IStorageService
+    {
+        public Task<UploadFileServiceResponse> Upload(UploadFileRequest request);
+    }
+}
