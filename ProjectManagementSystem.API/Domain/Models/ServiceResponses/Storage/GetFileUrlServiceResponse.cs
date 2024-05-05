848f17f80d650a1402c46bbb02e4c78a30121156
alirezakamranian <kamranianalireza1090@gmail.com>
2024-05-05 02:25:44 -0700

    Feat:Add getFileUrl service

---
+++ ProjectManagementSystem.API/Domain/Models/ServiceResponses/Storage/GetFileUrlServiceResponse.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models.ServiceResponses.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.ServiceResponses.Storage
+{
+    public class GetFileUrlServiceResponse(string status):ServiceResponseBase(status)
+    {
+        public string Url { get; set; }
+    }
+    public class GetFileUrlServiceResponseStatus:ServiceResponseStatusBase
+    {
+    }
+}
